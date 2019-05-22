a9541a83e0455a0501700ef68ffd080c4c8ea97e
Main Desktop <mmoretticom@gmail.com>
2019-05-22 00:29:58 -0300

    testing random api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const uuid = require('uuid')
+const request = require('request')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-app.get('/', (req, res) => res.send(uuid()))
+
+app.get('/', async (req, res) => {
+    const _int = Math.floor(Math.random() * 59);
+    console.log(_int)
+    request.get('http://www.commitstrip.com/en/page/' + _int+ '/', (error, response, body) => {
+        const dom = new JSDOM(body)
+        const a = dom.window.document.querySelectorAll('.excerpt section a')
+        console.log(dom.window.document.querySelectorAll('.excerpt section a')[0])
+        console.log(a.innerHTML)
+        res.json(dom.window.document.querySelectorAll('.excerpt section a')[0])
+
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
